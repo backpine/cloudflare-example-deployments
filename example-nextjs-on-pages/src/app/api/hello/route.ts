9b3d5a62ae1f43c1ec7b41482d270a6951c05eda
Matthew Sessions <matthewsessions@Matthews-MacBook-Pro.local>
2024-10-19 22:17:23 -0700

    added change

--- example-nextjs-on-pages/src/app/api/hello/route.ts
+++ example-nextjs-on-pages/src/app/api/hello/route.ts
@@ -4,7 +4,7 @@ import { getRequestContext } from '@cloudflare/next-on-pages'
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
-  let responseText = 'Hello there World bye'
+  let responseText = 'Hello there World'
 
   // In the edge runtime you can use Bindings that are available in your application
   // (for more details see:
