0e30ae7a554050f083c93ee3b251de985d1a7b6d
Matthew Sessions <matthewsessions@Matthews-MacBook-Pro.local>
2024-10-26 20:24:17 -0700

    added final project

--- example-hono-on-workers/src/index.ts
+++ example-hono-on-workers/src/index.ts
@@ -1,9 +1,17 @@
+import { users } from '@schema/drizzle'
+import { drizzle } from 'drizzle-orm/d1'
 import { Hono } from 'hono'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono changed!')
+})
+
+app.get('/users', async (c) => {
+  const db = drizzle(c.env.DB)
+  const result = await db.select().from(users).all()
+  return c.json(result)
 })
 
 export default app
